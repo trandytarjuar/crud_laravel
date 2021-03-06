7e2e38cd0fdca3ac8a582dbcc178fd51fa1be975
trandy <trandy.pencout@gmail.com>
2021-03-06 16:55:07 +0700

    tes

--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -21,35 +21,35 @@
 <script>
 if ($('#lineChartz').length ){	
 			
-    var ctx = document.getElementById("lineChartz");
-    var lineChartz = new Chart(ctx, {
-        type: 'line',
-        data: {
-        labels: ["datetime"],
-        datasets: [{
-            label: "value1",
-            backgroundColor: "rgba(38, 185, 154, 0.31)",
-            borderColor: "rgba(38, 185, 154, 0.7)",
-            pointBorderColor: "rgba(38, 185, 154, 0.7)",
-            pointBackgroundColor: "rgba(38, 185, 154, 0.7)",
-            pointHoverBackgroundColor: "#fff",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointBorderWidth: 1,
-            data: [31, 74, 6, 39, 20, 85, 7]
-        }, {
-            label: "My Second dataset",
-            backgroundColor: "rgba(3, 88, 106, 0.3)",
-            borderColor: "rgba(3, 88, 106, 0.70)",
-            pointBorderColor: "rgba(3, 88, 106, 0.70)",
-            pointBackgroundColor: "rgba(3, 88, 106, 0.70)",
-            pointHoverBackgroundColor: "#fff",
-            pointHoverBorderColor: "rgba(151,187,205,1)",
-            pointBorderWidth: 1,
-            data: [82, 23, 66, 9, 99, 4, 2]
-        }]
-        },
-    });
-    
-    }
+            var ctx = document.getElementById("lineChartz");
+            var lineChartz = new Chart(ctx, {
+                type: 'line',
+                data: {
+                labels: ["2020-01-01","2020-01-02","2020-01-03","2020-01-04","2020-01-05"],
+                datasets: [{
+                    label: "value_1",
+                    backgroundColor: "rgba(38, 185, 154, 0.31)",
+                    borderColor: "rgba(38, 185, 154, 0.7)",
+                    pointBorderColor: "rgba(38, 185, 154, 0.7)",
+                    pointBackgroundColor: "rgba(38, 185, 154, 0.7)",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointBorderWidth: 1,
+                    data: [10, 8, 9, 5, 11]
+                }, {
+                    label: "value_2",
+                    backgroundColor: "rgba(3, 88, 106, 0.3)",
+                    borderColor: "rgba(3, 88, 106, 0.70)",
+                    pointBorderColor: "rgba(3, 88, 106, 0.70)",
+                    pointBackgroundColor: "rgba(3, 88, 106, 0.70)",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(151,187,205,1)",
+                    pointBorderWidth: 1,
+                    data: [5, 9, 10, 11, 10]
+                }]
+                },
+            });
+            
+            }
 </script>
 @endsection
